698ba3ba144d83e99d8fc9c4f9ba3141427844b0
Regan <sooboy@foxmail.com>
2022-11-18 12:02:23 +0000

    add charts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,8 @@
 <script>
-	import Header from './Header.svelte';
 	import './styles.css';
 </script>
 
 <div class="app">
-	<Header />
-
 	<main>
 		<slot />
 	</main>
@@ -31,21 +28,5 @@
 		box-sizing: border-box;
 	}
 
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
 
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
 </style>
